28e736f558832c52a9f1e5ccdb0dc2045c2d2030
kaua64 <kauarochagilgarcia50@gmail.com>
2026-06-08 20:48:43 -0300

    Implementa modelo de regressao logistica

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 colunas = [
     'Pregnancies',
@@ -17,14 +20,22 @@ df = pd.read_csv(
     names=colunas
 )
 
-print("Primeiras 5 linhas:")
-print(df.head())
+X = df.drop('Outcome', axis=1)
+y = df['Outcome']
 
-print("\nInformações:")
-print(df.info())
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
 
-print("\nEstatísticas:")
-print(df.describe())
+modelo = LogisticRegression(max_iter=1000)
 
-print("\nQuantidade de pacientes com diabetes:")
-print(df['Outcome'].value_counts())
+modelo.fit(X_train, y_train)
+
+previsoes = modelo.predict(X_test)
+
+acuracia = accuracy_score(y_test, previsoes)
+
+print("Acurácia do modelo:", round(acuracia * 100, 2), "%")
