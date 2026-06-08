0059b2bc06c64a576a154556450c8245915f5fa5
kaua64 <kauarochagilgarcia50@gmail.com>
2026-06-08 20:44:06 -0300

    Adiciona analise exploratoria dos dados

--- main.py
+++ main.py
@@ -17,4 +17,14 @@ df = pd.read_csv(
     names=colunas
 )
 
+print("Primeiras 5 linhas:")
 print(df.head())
+
+print("\nInformações:")
+print(df.info())
+
+print("\nEstatísticas:")
+print(df.describe())
+
+print("\nQuantidade de pacientes com diabetes:")
+print(df['Outcome'].value_counts())
